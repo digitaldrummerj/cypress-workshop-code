908245c91f178c993227e02f6a04246144c8c654
Justin James <digitaldrummerj@gmail.com>
2019-10-23 07:29:12 -0700

    add: cypress workshop files

---
+++ cypress/integration/04-reset-state/spec.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+beforeEach(() => {
+  // application should be running at port 3000
+  // and the "localhost:3000" is set as "baseUrl" in "cypress.json"
+  cy.visit('/')
+})
+it('loads', () => {
+  cy.contains('h1', 'todos')
+})
+/**
+ * Adds a todo item
+ * @param {string} text
+ */
+const addItem = text => {
+  cy.get('.new-todo').type(`${text}{enter}`)
+}
+it('adds two items', () => {
+  addItem('first item')
+  addItem('second item')
+  cy.get('li.todo').should('have.length', 2)
+})
