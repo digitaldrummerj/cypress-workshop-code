610162ca2cb267602e79f1eac9f2e4d7016b8bee
Justin James <digitaldrummerj@gmail.com>
2019-10-24 07:53:46 -0700

    add: percy

--- cypress/integration/11-layout-testing/spec.js
+++ cypress/integration/11-layout-testing/spec.js
@@ -1,13 +1,41 @@
 /// <reference types="cypress" />
-beforeEach(() => {
-  cy.visit('/')
-})
+// https://docs.percy.io/docs/cypress
 
-it('adds 2 todos', () => {
-  cy.get('.new-todo')
-    .type('learn testing{enter}')
-    .type('be cool{enter}')
-  cy.get('.todo-list li').should('have.length', 2)
-})
+// 1.
+// npm install --save-dev @percy/cypress
 
-// add more tests to cover more application source code
+// 2.
+// At the top of cypress/support/commands.js
+// import '@percy/cypress';
+
+// 3.
+// In cypress/plugins/index.js
+// let percyHealthCheck = require('@percy/cypress/task')
+
+// module.exports = (on, config) => {
+//   on("task", percyHealthCheck);
+// };
+
+// 4.
+// # Windows
+// $ set PERCY_TOKEN=<your token here>
+
+// # Unix
+// $ export PERCY_TOKEN=<your token here>
+
+// 5.
+// add to cypress.json
+// "testFiles": "**/11*/spec.js",
+
+// 6.
+// npx percy exec -- cypress run
+
+describe('Integration test with visual testing', function() {
+  it('Loads the homepage', function() {
+    // Load the page or perform any other interactions with the app.
+    cy.visit('/');
+
+    // Take a snapshot for visual diffing
+    cy.percySnapshot();
+  });
+});
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,51 +10,50 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+let percyHealthCheck = require('@percy/cypress/task');
 
 /* eslint-disable no-console */
 /* global Promise */
-const fs = require('fs')
-const path = require('path')
-const debug = require('debug')('testing-workshop-cypress')
+const fs = require('fs');
+const path = require('path');
+const debug = require('debug')('testing-workshop-cypress');
 
 const findRecord = title => {
-  const dbFilename = path.join(__dirname, '..', '..', 'todomvc', 'data.json')
-  const contents = JSON.parse(fs.readFileSync(dbFilename))
-  const todos = contents.todos
-  return todos.find(record => record.title === title)
-}
+  const dbFilename = path.join(__dirname, '..', '..', 'todomvc', 'data.json');
+  const contents = JSON.parse(fs.readFileSync(dbFilename));
+  const todos = contents.todos;
+  return todos.find(record => record.title === title);
+};
 
 const hasRecordAsync = (title, ms) => {
-  const delay = 50
+  const delay = 50;
   return new Promise((resolve, reject) => {
     if (ms < 0) {
-      return reject(new Error(`Could not find record with title "${title}"`))
+      return reject(new Error(`Could not find record with title "${title}"`));
     }
-    const found = findRecord(title)
+    const found = findRecord(title);
     if (found) {
-      return resolve(found)
+      return resolve(found);
     }
     setTimeout(() => {
-      hasRecordAsync(title, ms - delay).then(resolve, reject)
-    }, 50)
-  })
-}
+      hasRecordAsync(title, ms - delay).then(resolve, reject);
+    }, 50);
+  });
+};
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // "cy.task" can be used from specs to "jump" into Node environment
   // and doing anything you might want. For example, checking "data.json" file!
   on('task', {
-    hasSavedRecord (title, ms = 3000) {
-      debug('inside task')
-      console.log(
-        'looking for title "%s" in the database (time limit %dms)',
-        title,
-        ms
-      )
-      return hasRecordAsync(title, ms)
-    }
-  })
+    hasSavedRecord(title, ms = 3000) {
+      debug('inside task');
+      console.log('looking for title "%s" in the database (time limit %dms)', title, ms);
+      return hasRecordAsync(title, ms);
+    },
+  });
+
+  on('task', percyHealthCheck);
 
   // code coverage tasks
   // @see https://on.cypress.io/code-coverage
@@ -64,10 +63,10 @@ module.exports = (on, config) => {
 
   // `config` is the resolved Cypress config
   // see https://on.cypress.io/configuration-api
-  config.fixturesFolder = 'cypress/fixtures'
-  config.modifyObstructiveCode = false
-  return Promise.resolve(config)
-}
+  config.fixturesFolder = 'cypress/fixtures';
+  config.modifyObstructiveCode = false;
+  return Promise.resolve(config);
+};
 
 // init for cypress-plugin-snapshots
 // const snapshotsPlugin = require('cypress-plugin-snapshots/plugin')
